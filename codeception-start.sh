32736168b6e6dc804e741e13c0235ebcf7c0953e
Elias Van Ootegem <elias.vanootegem@wimionline.be>
2014-10-17 12:01:06 +0200

    Made start script quiet (start driver and Xvfb as daemon)\ Replaced hard-coded bin paths with $(which <bin-name>)\ Remove pid files when processes are stopped

--- codeception-start.sh
+++ codeception-start.sh
@@ -1,37 +1,63 @@
 #!/usr/bin/env bash
 startxvfb() {
-    XVFB=/usr/bin/Xvfb
+    XVFB=$(which Xvfb)
     XVFBARGS=":99 -screen 0 1024x768x24 -fbdir /var/run -ac"
     PIDFILE=/var/run/xvfb.pid
     case "$1" in
         start)
             echo "Starting Xvfb"
-                start-stop-daemon --start --quiet --pidfile $PIDFILE --make-pidfile --background --exec $XVFB -- $XVFBARGS
+            sudo start-stop-daemon --start --quiet --pidfile $PIDFILE --make-pidfile --background --exec $XVFB -- $XVFBARGS
             ;;
         stop)
             echo "Stopping Xvfb"
-            start-stop-daemon --stop --quiet --pidfile $PIDFILE
+            sudo start-stop-daemon --stop --quiet --pidfile $PIDFILE
+            if [ -e $PIDFILE ]
+                then sudo rm -f $PIDFILE
+            fi
             ;;
         *)
             echo "ERROR"
             exit 1
     esac
 }
+
+startselenium() {
+    PIDFILE=/var/run/selenium.pid
+    JAVA=$(which java)
+    JAVA_ARGS="-Dwebdriver.chrome.driver=$CHROME_PATH -jar $SELENIUM_PATH"
+    case "$1" in
+        start)
+            echo "Starting selenium"
+            sudo start-stop-daemon --start --quiet --pidfile $PIDFILE --make-pidfile --background --exec $JAVA -- $JAVA_ARGS
+            ;;
+        stop)
+            echo "Stopping selenium"
+            sudo start-stop-daemon --stop --quiet --pidfile $PIDFILE
+            if [ -e $PIDFILE ]
+                then sudo rm -f $PIDFILE
+            fi
+            ;;
+        *)
+            echo "ERROR"
+            exit 1
+    esac
+}
+
 if [[ $# -eq 0 ]]; then
     echo 'No arguments...'
     exit 1;
 fi
 CHROME_PATH="/full/path/to/chromedriver"
-SELENIUM_PATH="/full/pat/to/selenium-server-standalone-X.XX.X.jar"
+SELENIUM_PATH="/full/path/to/selenium-server-standalone-X.XX.X.jar"
 case "$1" in
     start)
         echo 'Starting test env...'
         startxvfb $1
-        java -Dwebdriver.chrome.driver=$CHROME_PATH -jar $SELENIUM_PATH
+        startselenium $1
         ;;
     stop)
         startxvfb $1
-        "Stopped xvfb"
+        startselenium $1
         exit 0
         ;;
      *)
