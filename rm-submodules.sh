bf12963ae10054479db854fbd6459b8452758f4c
Elias Van Ootegem <elias.vanootegem@wimionline.be>
2014-10-23 08:40:03 +0200

    Retab + more output

--- rm-submodules.sh
+++ rm-submodules.sh
@@ -1,27 +1,27 @@
 #!/usr/bin/env bash
 submodpath() {
-	SUB_PATH=${1}
-	for subdir in $(ls $SUB_PATH)
-	do
-		config="$MOD_ROOT/$SUB_PATH/$subdir/config"
-		if [ -f $config ] ; then
-			echo "Found submodule $SUB_PATH/$subdir"
-			SUBMODULES+=("$SUB_PATH/$subdir")
-		else
-			submodpath "$SUB_PATH/$subdir"
-		fi
-	done
+    SUB_PATH=${1}
+    for subdir in $(ls $SUB_PATH)
+    do
+        config="$MOD_ROOT/$SUB_PATH/$subdir/config"
+        if [ -f $config ] ; then
+            echo "Found submodule $SUB_PATH/$subdir"
+            SUBMODULES+=("$SUB_PATH/$subdir")
+        else
+            submodpath "$SUB_PATH/$subdir"
+        fi
+    done
 }
 if [ ! -d .git ] ; then
-	echo "No .git directory found"
-	exit 1
+    echo "No .git directory found"
+    exit 1
 fi
 
 git stash && git pull && git submodule update --init
 
 if [ ! -d .git/modules ] ; then
-	echo "No modules found"
-	exit 0
+    echo "No modules found"
+    exit 0
 fi
 
 MOD_ROOT=".git/modules"
@@ -31,29 +31,30 @@ SUBMODULES=()
 for submod in $(ls $MOD_ROOT)
 do
     TO_ADD+=($submod)
-	submodpath "$submod"
+    submodpath "$submod"
 done
 
 for module in "${SUBMODULES[@]}"
 do
-	echo "removing submodule $module"
-	git rm --cached "$module"
-	if [ -f "$module/.git" ] ; then
-		rm "$module/.git"
-	else
-		echo "No .git ref-file found"
-	fi
+    echo "removing submodule $module"
+    git rm --cached "$module"
+    if [ -f "$module/.git" ] ; then
+        rm "$module/.git"
+    else
+        echo "No .git ref-file found"
+    fi
 done
 
 if [ -f .gitmodules ] ; then
-	echo "Regmove .gitmodules file"
-	rm .gitmodules
+    echo "Regmove .gitmodules file"
+    rm .gitmodules
 fi
 
 echo "Adding submodules to main repo"
 for add_path in "${TO_ADD[@]}"
 do
-	git add "$add_path"
+    echo "Add submodule(s) in path $add_path"
+    git add "$add_path"
 done
 echo "Done... now commit && push!"
 exit 0
