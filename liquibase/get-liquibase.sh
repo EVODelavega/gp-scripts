7df833b9ab34dd3de3146a802d4f1d07ba0f379e
Elias Van Ootegem <elias.vanootegem@wimionline.be>
2015-07-14 15:44:56 +0200

    Add simple liquibase setup scripts

---
+++ liquibase/get-liquibase.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+connector_name=mysql-connector-java-5.1.35
+if [ ! -d liquibase ]; then
+    #create liquibase dir
+    mkdir liquibase
+    #get the liquibase tarball
+    wget https://github.com/liquibase/liquibase/releases/download/liquibase-parent-3.3.5/liquibase-3.3.5-bin.tar.gz
+    tar xvfz liquibase-3.3.5-bin.tar.gz -C liquibase
+    #get the DB driver tar
+    wget http://dev.mysql.com/get/Downloads/Connector-J/mysql-connector-java-5.1.35.tar.gz
+    tar xvfz "${connector_name}.tar.gz" -C liquibase
+    #move the jar to liquibase dir
+    mv "liquibase/${connector_name}/${connector_name}-bin.jar" liquibase/
+    #cleanup
+    rm -Rf "liquibase/${connector_name}"
+    rm liquibase*.tar.gz
+    rm mysql-connector*.tar.gz
+    # copy config script
+    cp setup.ph liquibase/
+fi
---
+++ liquibase/setup.pl
@@ -0,0 +1,70 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+use Getopt::Long;
+use Pod::Usage;
+
+my $filename = 'liquibase.properties';
+
+my %opts=(
+    filename    => \$filename,
+    driver      => 'com.mysql.jdbc.Driver',
+    classpath   => './mysql-connector-java-5.1.35-bin.jar',
+    url         => 'jdbc:mysql://127.0.0.1/my_db',
+    username    => 'root',
+    password    => '',
+    help        => 0
+);
+
+GetOptions(\%opts, 'filename=s', 'driver=s', 'classpath=s', 'url|server=s', 'username=s', 'password=s', 'help|?')
+    or pod2usage(-existval => 1, -verbose => 99);
+
+pod2usage(-exitval => 0, -verbose => 1) if $opts{help};
+
+# GetOptions('filename=s' => \$filename);
+
+open(my $fh, '>:encoding(UTF-8)', $filename)
+    or die "Failed to open '$filename': $!\n";
+
+for my $key (keys %opts) {
+    if ($key ne 'filename' && $key ne 'help') {
+        print $fh "$key: $opts{$key}\n"
+    }
+}
+
+close $fh;
+print "Done\n";
+
+__END__
+
+=head1 NAME
+
+setup - Usage
+
+=head1 SYNOPSIS
+
+setup [options] - Generate liquibase properties file
+
+=head1 OPTIONS
+
+    Options:
+        --help                                              : Display help message
+        --filename=liquibase.properties                     : The output file (default is liquibase.properties in current directory)
+        --driver=com.mysql.jdbc.Driver                      : The DB driver used by liquibase (class)
+        --classpath=./mysql-connector-java-5.1.35-bin.jar   : The location of the DB driver jar
+        --url=jdbc:mysql://127.0.0.1/my_db                  : The DB to connect to
+        --username=root                                     : user (DB credentials)
+        --password=''                                       : password (DB credentials)
+
+
+=over 8
+
+Enjoy
+
+=back
+
+=head1 DESCRIPTION
+
+B<this program> will generate the liquibase.properties file with the params given. Default is to create it in the current directory under the liquibase.properties name
+
+=cut
