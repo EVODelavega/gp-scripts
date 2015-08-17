7ab315fb2bbb18a4f1529618c8f8d58b4d8fe88e
EVODelavega <eliasvo@gmail.com>
2015-08-17 17:43:08 +0100

    Added perl collatz snippet

---
+++ collatz/perl.pl
@@ -0,0 +1,17 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+
+sub r {
+    my ($n) = @_;
+    print "$n\n";
+    if ($n == 1) {
+        return $n;
+    }
+    if ($n%2) {
+        return r(($n*3)+1);
+    }
+    return r($n/2);
+}
+print "Basic collatz fun -recursive perl function\n";
+r(15);
