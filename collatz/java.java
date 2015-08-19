501e6c942c06455c536728568913182514d746f9
Elias Van Ootegem <eliasvo@gmail.com>
2015-08-19 07:19:10 +0200

    Add evil language, too

---
+++ collatz/java.java
@@ -0,0 +1,18 @@
+class Collatz
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Basic collatz fun - recursive Java function");
+        r(15);
+    }
+
+    private static int r(int n)
+    {
+        System.out.printf("%d%n", n);
+        if (n <= 1)
+            return 1;
+        if (n%2 == 0)
+            return r(n/2);
+        return r((n*3)+1);
+    }
+}
