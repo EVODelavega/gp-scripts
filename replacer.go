6f80ee6bd1046fe8ef6583cc15ce048e641a60d9
EVODelavega <eliasvo@gmail.com>
2015-09-17 16:04:16 +0100

    Replace tab indents with spaces (some vim plugin is converting spaces into tabs on save)

--- replacer.go
+++ replacer.go
@@ -1,12 +1,12 @@
 package main
 
 import (
-	"bufio"
-	"bytes"
-	"fmt"
-	"io"
-	"os"
-	"strings"
+    "bufio"
+    "bytes"
+    "fmt"
+    "io"
+    "os"
+    "strings"
 )
 
 /**
@@ -16,48 +16,48 @@ import (
  */
 
 func main() {
-	argv := os.Args[1:]
-	if len(argv) != 4 {
-		fmt.Println(os.Stderr, "Expected 4 arguments")
-		return
-	}
-	file, err := os.Open(argv[0])
-	if err != nil {
-		panic(err)
-	}
-	defer file.Close()
+    argv := os.Args[1:]
+    if len(argv) != 4 {
+        fmt.Println(os.Stderr, "Expected 4 arguments")
+        return
+    }
+    file, err := os.Open(argv[0])
+    if err != nil {
+        panic(err)
+    }
+    defer file.Close()
 
-	f, err := os.Create(argv[1])
-	if err != nil {
-		panic(err)
-	}
-	defer f.Close()
-	w := bufio.NewWriter(f)
+    f, err := os.Create(argv[1])
+    if err != nil {
+        panic(err)
+    }
+    defer f.Close()
+    w := bufio.NewWriter(f)
 
-	reader := bufio.NewReader(file)
-	buffer := bytes.NewBuffer(make([]byte, 0))
-	var (
-		part   []byte
-		prefix bool
-	)
-	for {
-		if part, prefix, err = reader.ReadLine(); err != nil {
-			if err != io.EOF {
-				fmt.Fprintln(os.Stderr, "error reading input file: ", err)
-			}
-			break
-		}
-		buffer.Write(part)
-		if !prefix {
-			s := buffer.String()
-			_, err := w.WriteString(strings.Replace(s, argv[2], argv[3], -1))
-			if err != nil {
-				fmt.Fprintln(os.Stderr, "error writing file: ", err)
-				break
-			}
-			w.Flush()
-			buffer.Reset()
-		}
-	}
+    reader := bufio.NewReader(file)
+    buffer := bytes.NewBuffer(make([]byte, 0))
+    var (
+        part   []byte
+        prefix bool
+    )
+    for {
+        if part, prefix, err = reader.ReadLine(); err != nil {
+            if err != io.EOF {
+                fmt.Fprintln(os.Stderr, "error reading input file: ", err)
+            }
+            break
+        }
+        buffer.Write(part)
+        if !prefix {
+            s := buffer.String()
+            _, err := w.WriteString(strings.Replace(s, argv[2], argv[3], -1))
+            if err != nil {
+                fmt.Fprintln(os.Stderr, "error writing file: ", err)
+                break
+            }
+            w.Flush()
+            buffer.Reset()
+        }
+    }
 
 }
