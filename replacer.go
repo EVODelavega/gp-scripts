495a184087b39edbd6e26bfeff9278ce4bf9b3c3
EVODelavega <eliasvo@gmail.com>
2015-09-17 17:26:16 +0100

    divide logic into small functions, some documentation added

--- replacer.go
+++ replacer.go
@@ -20,58 +20,102 @@ import (
  * ./replace -unsafe in.log out.log search replace
  *
  */
-func main() {
+
+//convert read buffer to string
+func bufferToString(buffer *bytes.Buffer, unsafePtr *bool) string {
+    defer buffer.Reset()//ensure buffer is reset
+    if !*unsafePtr {
+        return buffer.String()
+    }
+    bb := buffer.Bytes()
+    s := *(*string)(unsafe.Pointer(&bb))
+    return s
+}
+
+//help, or invalid flag
+func usage() {
+    fmt.Printf("Usage of %s:\n", os.Args[0])
+    fmt.Println("    [-unsafe] inputfile outfile search-string replace-string")
+    fmt.Println("\nAvailable options:")
+    flag.PrintDefaults()
+    fmt.Printf("\nThe -unsafe flag can increase performance. The performance gain is, however, marginal\n")
+    fmt.Println("and is generally not worth the added risks, hence the name: unsafe")
+}
+
+//open files taken from argv
+func openFiles(argv []string) (*os.File, *os.File) {
+    inFile, err := os.Open(argv[0])
+    if err != nil {
+        panic(err)
+    }
+
+    outFile, err := os.Create(argv[1])
+    if err != nil {
+        inFile.Close()//close here
+        panic(err)
+    }
+    return inFile, outFile
+}
+
+//open reader and writer
+func openReadWrite(inFile *os.File, outFile *os.File) (*bufio.Reader, *bufio.Writer) {
+    return bufio.NewReader(inFile), bufio.NewWriter(outFile)
+}
+
+func getArguments() (*bool, []string) {
     unsafePtr := flag.Bool("unsafe", false, "use unsafe byte-to-string conversion")
+    helpPtr := flag.Bool("help", false, "Display help message")
+    flag.Usage = usage;
     flag.Parse()
+    if *helpPtr {
+        usage()
+        return nil, flag.Args()
+    }
     argv := flag.Args()
     if len(argv) != 4 {
-        fmt.Println(os.Stderr, "Expected 4 arguments")
+        fmt.Fprintln(os.Stderr, "Expected 4 arguments")
+        return nil, argv
+    }
+    return unsafePtr, argv
+}
+
+//main function
+func main() {
+    unsafePtr, argv := getArguments()
+    if unsafePtr == nil {
         return
     }
-    file, err := os.Open(argv[0])
-    if err != nil {
-        panic(err)
-    }
+    file, f := openFiles(argv)
     defer file.Close()
-
-    f, err := os.Create(argv[1])
-    if err != nil {
-        panic(err)
-    }
     defer f.Close()
-    w := bufio.NewWriter(f)
 
-    reader := bufio.NewReader(file)
+    reader, w := openReadWrite(file, f)
+
     buffer := bytes.NewBuffer(make([]byte, 0))
     var (
         part   []byte
         prefix bool
     )
     for {
+        var err error
         if part, prefix, err = reader.ReadLine(); err != nil {
             if err != io.EOF {
                 fmt.Fprintln(os.Stderr, "error reading input file: ", err)
+            } else {
+                fmt.Println("File processed")
             }
             break
         }
         buffer.Write(part)
         buffer.WriteString("\n") //add the \n before writing to the new file:wq
         if !prefix {
-            var s string
-            if !*unsafePtr {
-                s = buffer.String()
-            } else {
-                //unsafe, but perhaps a quicker alternative:
-                bb := buffer.Bytes()
-                s = *(*string)(unsafe.Pointer(&bb))
-            }
+            s := bufferToString(buffer, unsafePtr)
             _, err := w.WriteString(strings.Replace(s, argv[2], argv[3], -1))
             if err != nil {
                 fmt.Fprintln(os.Stderr, "error writing file: ", err)
                 break
             }
             w.Flush()
-            buffer.Reset()
         }
     }
 
