0a786b4cc88ffdd1b6ee764c869312b9305fe576
EVODelavega <eliasvo@gmail.com>
2015-10-15 18:18:26 +0100

    Deleting branches is so tedious...

---
+++ delete-branch.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+current_branch=$(git branch | grep '*' | awk '{print $NF;}')
+
+[[ $# -gt 0 ]] || exit 1
+
+delete_branch="$1"
+current_branch=$(git branch | grep '*' | awk '{print $NF;}')
+
+if [ "$delete_branch" == "$current_branch" ]; then
+    echo "Cannot delete the branch you're on..."
+    exit 1
+fi
+
+if $(git branch | grep -q "$delete_branch"); then
+    git branch -D "$delete_branch"
+fi
+
+git push origin :"$delete_branch"
+
